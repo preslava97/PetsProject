309879e4a741ec5cd5455d870ed2ccd5b9a8e9ee
ayhan21 <ayhan.a.yakubov@gmail.com>
2019-11-23 17:18:45 +0200

    Added Components Contacts, FAQ and routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,15 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {MatSidenavModule} from '@angular/material/sidenav';
+import { FAQComponent } from './faq/faq.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
+    FAQComponent,
+    ContactsComponent,
   ],
   imports: [
     BrowserModule,
