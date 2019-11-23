68d4adb1c8cdc2f0ae204fcfa8aefe3e53d2bf04
ayhan21 <ayhan.a.yakubov@gmail.com>
2019-11-23 20:12:58 +0200

    Fixed routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { FAQComponent } from './faq/faq.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: 'home', pathMatch: 'full'
+  },
+  {
+    path: 'home', component: HomepageComponent
+  },
+  {
+    path: 'contacts', component: ContactsComponent
+  },
+  {
+    path: 'faq', component: FAQComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
