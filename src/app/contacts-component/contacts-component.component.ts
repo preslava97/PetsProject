b762f8f10d8701173c46f7dc0c59a1626d997f18
ayhan21 <ayhan.a.yakubov@gmail.com>
2019-11-23 14:00:00 +0200

    Created FAQComponent, ContactsComponent

---
+++ src/app/contacts-component/contacts-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contacts-component',
+  templateUrl: './contacts-component.component.html',
+  styleUrls: ['./contacts-component.component.scss']
+})
+export class ContactsComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
