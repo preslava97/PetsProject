4cbabd6fda76c88d9ae8ad3badb0de4d8d99d516
atev40 <volvov401999@abv.bg>
2019-12-04 01:42:04 +0200

    add button back  in accessories,food

--- src/app/catalogue/accessories/accessories.component.ts
+++ src/app/catalogue/accessories/accessories.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-accessories',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccessoriesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
-
+  goBack(): void {
+    this.router.navigate(['catalogue']);
+  }
 }
--- src/app/catalogue/food/food.component.ts
+++ src/app/catalogue/food/food.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-food',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./food.component.scss']
 })
 export class FoodComponent implements OnInit {
-  
-  constructor() { }
+  constructor(private router: Router) { }
+
 
   ngOnInit() {
   }
+    goBack(): void {
+      this.router.navigate(['catalogue']);
+    }
 
-}
+  }
+
+
